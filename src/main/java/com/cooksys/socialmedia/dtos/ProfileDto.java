29b8899b4f64e2e3abb5404832a5d1a926c6fcab
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-06-28 10:47:26 -0500

    Update ProfileDto.java

--- src/main/java/com/cooksys/socialmedia/dtos/ProfileDto.java
+++ src/main/java/com/cooksys/socialmedia/dtos/ProfileDto.java
@@ -1,14 +1,11 @@
 package com.cooksys.socialmedia.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class ProfileDto {
     private String email;
