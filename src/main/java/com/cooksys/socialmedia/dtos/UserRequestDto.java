1b25321f5a553865753a843c8e0e7800badd6864
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-06-28 10:50:59 -0500

    Update UserRequestDto.java

--- src/main/java/com/cooksys/socialmedia/dtos/UserRequestDto.java
+++ src/main/java/com/cooksys/socialmedia/dtos/UserRequestDto.java
@@ -1,12 +1,10 @@
 package com.cooksys.socialmedia.dtos;
 
 import com.cooksys.socialmedia.entities.Profile;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class UserRequestDto {
     private CredentialsDto credentials;
