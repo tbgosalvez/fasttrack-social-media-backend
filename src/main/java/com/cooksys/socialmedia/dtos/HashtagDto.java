95021e00d5b7676e5dc28f5dc9afbfe544d5940d
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-06-28 10:44:09 -0500

    Update HashtagDto.java

--- src/main/java/com/cooksys/socialmedia/dtos/HashtagDto.java
+++ src/main/java/com/cooksys/socialmedia/dtos/HashtagDto.java
@@ -1,14 +1,14 @@
 package com.cooksys.socialmedia.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class HashtagDto {
     private String label;
-    private String firstUsed;
-    private String lastUsed;
+    private Timestamp firstUsed;
+    private Timestamp lastUsed;
 }
