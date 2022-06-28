00eba48de44e66b1b20d553f9b23a53cbfab87fe
Helena Makendengue <HelenaMakens1013@gmail.com>
2022-06-28 10:37:49 -0500

    Update ErrorDto.java

--- src/main/java/com/cooksys/socialmedia/dtos/ErrorDto.java
+++ src/main/java/com/cooksys/socialmedia/dtos/ErrorDto.java
@@ -2,8 +2,10 @@ package com.cooksys.socialmedia.dtos;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class ErrorDto {
 	
