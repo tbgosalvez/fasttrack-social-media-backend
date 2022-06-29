da210aacd45096c7e0f2753c01d4361045425ebd
Timothy-Brian Gosalvez <tbgosalvez@gmail.com>
2022-06-29 19:57:04 -0400

    added GET tags endpoint

--- src/main/java/com/cooksys/socialmedia/services/HashtagService.java
+++ src/main/java/com/cooksys/socialmedia/services/HashtagService.java
@@ -1,5 +1,10 @@
 package com.cooksys.socialmedia.services;
 
+import com.cooksys.socialmedia.dtos.HashtagDto;
+
+import java.util.List;
+
 public interface HashtagService {
 
+    List<HashtagDto> getAllTags();
 }
